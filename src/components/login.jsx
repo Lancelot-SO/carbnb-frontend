9cac52a8cd994b196b95beaa2f1b950860829263
Ellon <mordecaimwema11@gmail.com>
2023-06-08 13:07:34 +0300

    add ui for login/signup page

---
+++ src/components/login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser, registerUser } from '../redux/users/users';
+
+const Login = () => {
+  const [username, setUserName] = useState('');
+
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state?.user.status);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    dispatch(loginUser(username));
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    dispatch(registerUser(username));
+  };
+
+  return (
+    <div className="form-container-login">
+      <form onSubmit={handleLogin}>
+        <div className="login-logo">
+        </div>
+        <input onChange={(e) => setUserName(e.target.value)} type="username" name="username" value={username} id="username" placeholder="Username:" required />
+        <input className="login-btn" type="submit" value="Login" />
+      </form>
+      <p>Don&apos;t have account?</p>
+      <a onClick={handleRegister} href="/signup" alt="create new account" className="font-bold sign-up">Sign up</a>
+      <p id="message">
+        {' '}
+        {error && <p>{error}</p>}
+      </p>
+    </div>
+  );
+};
+
+export default Login;
