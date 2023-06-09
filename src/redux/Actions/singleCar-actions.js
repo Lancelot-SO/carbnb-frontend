9432dfefaca801821c1380f9a70995ac72da3dc5
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-06-09 12:55:23 +0100

    Add single Cars action and slice

---
+++ src/redux/Actions/singleCar-actions.js
@@ -0,0 +1,36 @@
+import { singleCarActions } from '../Slices/singleCar-slice';
+
+export const fetchSingleCar = (id) => async (dispatch) => {
+  const response = await fetch(`http://127.0.0.1:3000/api/v1/car/${id}`);
+  const data = await response.json();
+  console.log(data);
+
+  const { car } = data;
+  dispatch(singleCarActions.singleCar(car));
+};
+
+export const createNewCar = async (
+  userId,
+  name,
+  model,
+  description,
+  price,
+  image,
+) => {
+  const response = await fetch(
+    `http://127.0.0.1:3000/api/v1/car/${userId}/${name}/${model}/${description}/${price}/${image}`,
+    {
+      method: 'POST',
+    },
+  );
+  const data = await response.json();
+  console.log(data);
+};
+
+export const deleteCar = async (id) => {
+  const response = await fetch(`http://127.0.0.1:3000/api/v1/car/${id}`, {
+    method: 'DELETE',
+  });
+  const data = await response.json();
+  console.log(data);
+};
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './Slices/user-slice';
 import carReducer from './Slices/car-slice';
+import singleCarReducer from './Slices/singleCar-slice';
 
 const store = configureStore({
-  reducer: { userSlice: userReducer, carSlice: carReducer },
+  reducer: {
+    userSlice: userReducer,
+    carSlice: carReducer,
+    singleCarSlice: singleCarReducer,
+  },
 });
 
 export default store;
