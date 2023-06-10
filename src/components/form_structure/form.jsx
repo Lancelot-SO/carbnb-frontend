3925014eafc16cdad7cdc08f3cb7d5ef139f5bdd
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-06-10 09:44:09 +0500

    updated Form button

--- src/components/form_structure/form.jsx
+++ src/components/form_structure/form.jsx
@@ -2,14 +2,28 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { fetchUser } from '../../redux/Actions/user-actions';
+
 import './form.css';
 
 function SampleForm() {
   const [price, setPrice] = useState(5);
+  // const [username, setUsername] = useState('');
+
+  // const handleUserChange = (event) => {
+  //   setUsername(event.target.value);
+  // };
 
   const handlePriceChange = (event) => {
     setPrice(event.target.value);
   };
+
+  // const dispatch = useDispatch();
+
+  // const currUser = dispatch(fetchUser(username));
+
+  const currUser = ''; // instead of this, we will use Redux selector to check if user already exists.
 
   return (
     <>
@@ -18,7 +32,7 @@ function SampleForm() {
           label="Username"
           className="mb-3"
         >
-          <Form.Control placeholder="Name" name="user[username]" />
+          <Form.Control placeholder="Name" name="user[username]" /* onChange={handleUserChange} */ />
           <Form.Text muted className="ms-2">
             Username should include numbers and letters
           </Form.Text>
@@ -34,7 +48,7 @@ function SampleForm() {
           name="car[price]"
         />
 
-        <Button type="submit" variant="outline-success mt-2 sub-btn">Submit form</Button>
+        <Button type="submit" variant="outline-success mt-2 sub-btn">{currUser ? 'Login' : 'Register' }</Button>
 
       </Form>
     </>
