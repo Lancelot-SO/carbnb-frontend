aa9f2f9689feb8d15e34e58809bb2484ed4727df
Ellon <mordecaimwema11@gmail.com>
2023-06-12 21:06:05 +0300

    add styles for add car form

--- src/components/Cars/add-car.jsx
+++ src/components/Cars/add-car.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import './add-car.css';
 // import { createCar } from '../redux/motorcycles/motorcycles';
 
 const AddCar = () => {
