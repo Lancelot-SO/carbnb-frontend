e67ecd11d428b078328814697e5edd593a8b15af
Ellon <mordecaimwema11@gmail.com>
2023-06-15 00:12:31 +0300

    made redux changes for home actions

--- src/redux/Actions/car-actions.js
+++ src/redux/Actions/car-actions.js
@@ -1,19 +1,28 @@
-import { carActions } from '../Slices/car-slice';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-export const fetchcars = () => async (dispatch) => {
-  const response = await fetch('http://127.0.0.1:3000/api/v1/cars');
-  const data = await response.json();
-  console.log(data);
+export const fetchcars = createAsyncThunk(
+  'cars/fetchcars',
+  async () => {
+    try {
+      const res = await axios.get('http://127.0.0.1:3000/api/v1/cars');
+      return res.data.cars;
+    } catch (err) {
+      return err.message;
+    }
+  },
+);
 
-  const { cars } = data;
-  dispatch(carActions.allCars(cars));
-};
+`http://127.0.0.1:3000/api/v1/cars/${id}`
 
-export const fetchUserCars = (id) => async (dispatch) => {
-  const response = await fetch(`http://127.0.0.1:3000/api/v1/cars/${id}`);
-  const data = await response.json();
-  console.log(data);
-
-  const { cars } = data;
-  dispatch(carActions.allCars(cars));
-};
+export const fetchUserCars = createAsyncThunk(
+  'cars/fetchcars',
+  async (id) => {
+    try {
+      const res = await axios.get(`http://127.0.0.1:3000/api/v1/cars/${id}`);
+      return res.data;
+    } catch (err) {
+      return err.message;
+    }
+  },
+);
--- src/redux/Slices/car-slice.js
+++ src/redux/Slices/car-slice.js
@@ -1,18 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const carSlice = createSlice({
-  name: 'Cars',
-  initialState: {
-    cars: [],
-  },
+export const carSlice = createSlice({
+  name: 'cars',
+  initialState: [],
   reducers: {
-    allCars(state, actions) {
-      // eslint-disable-next-line no-param-reassign
-      state.cars = actions.payload;
+    clearCars: (state) => {
+      state.length = 0;
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchcars.fulfilled, (state, action) => {
+        const ids = Object.keys(action.payload);
+        const newState = ids.map((id) => ({
+          id: action.payload[id].id,
+          user: action.payload[id].user_id,
+          name: action.payload[id].name,
+          description: action.payload[id].description,
+          imageUrl: action.payload[id].image,
+          price: action.payload[id].price,
+          model: action.payload[id].model,
+        }));
+        return newState;
+      });
   },
 });
 
-export const carActions = carSlice.actions;
+const { reducer } = carSlice;
+export const { clearCars } = carSlice.actions;
 
-export default carSlice.reducer;
+export default reducer;
